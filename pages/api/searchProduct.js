185c3952b01921c62c2e060c67a18262063f178a
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-15 16:09:12 +0100

    commit to main

--- pages/api/searchProduct.js
+++ pages/api/searchProduct.js
@@ -3,9 +3,10 @@
 import { client } from '../../lib/client';
 
 export default async function handler(req, res) {
+    console.log(req)
 try {
     // Fetch products from Sanity based on the searched name
-    const products = await client.fetch(`*[_type in ["product", "mattress", "chair", "bed", "bedroomset", "diningset", "jatifurniture", "multiplepurposes", "officetable", "sofa", "sofabed", "tvcabinet"] && name match "${req.body}*"]`);
+    const products = await client.fetch(`*[_type in ["product", "mattress", "chair", "bed", "bedroomset", "diningset", "jatifurniture", "multiplepurposes", "officetable", "sofa", "sofabed", "tvcabinet"] && name match "${req.body.query}*"]`);
 console.log(products)
     res.json(products);
 } catch (error) {
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,9 @@
 "use client";
-import { useEffect } from 'react';
+import { useEffect, useRef,useState} from 'react';
 import React from 'react'
 import Link from 'next/link'
 import { useStateContext } from '../../../context/StateContext';
-import { AiOutlineShopping, AiOutlineWhatsApp, AiOutlineSearch } from 'react-icons/ai'
+import { AiOutlineShopping, AiOutlineWhatsApp, AiOutlineSearch ,AiOutlineClose} from 'react-icons/ai'
 import { Cart } from './'
 // import Layout from '../../src/app/components/Layout'; // Import the Layout component
 
@@ -11,11 +11,43 @@ function Navbar() {
 
   const { showCart, setShowCart, totalQuantities } = useStateContext();
   console.log(totalQuantities)
-  async function handleSearch(query) {
+  const [searchResults, setSearchResults] = useState(false);
+  const searchResultsRef = useRef(null);
+
+  const searchInputRef = useRef(null);
+  useEffect(() => {
+   
+
+      handleSearch()
 
 
 
-    
+
+
+
+
+
+  }, [])
+
+  async function handleSearch() {
+
+    let query;
+
+   
+      query =searchInputRef.current.value;
+      if (query.length === 0) {
+        searchResultsRef.current.innerHTML = '';
+        return;
+      }
+
+
+
+
+
+      query = encodeURIComponent(query)
+
+      console.log({query})
+
     const response = await fetch('/api/searchProduct', {
       method: 'POST',
       headers:
@@ -23,16 +55,15 @@ function Navbar() {
         'Content-Type': 'application/json',
 
       },
-      body: query,
-    }).then(response => response.json())
-      .then(data => {
-        console.log(data)
-        displayResults(data);
-      })
-      .catch(error => {
-        console.error('Error fetching search results:', error);
-      });
-    
+      body: JSON.stringify({query}),
+    })
+console.log(response)
+    if (response.statusCode === 500) return
+    const data = await response.json();
+    console.log(data)
+    if(data.length>0){setSearchResults(true)}
+    displayResults(data)
+
 
 
 
@@ -40,44 +71,27 @@ function Navbar() {
   }
 
 
-//   const searchInput = document.getElementById('searchInput');
-//   const searchResults = document.getElementById('searchResults');
-//   let query;
 
-//   searchInput?.addEventListener('keypress', (event) => {
-//     query = searchInput.value.trim();
-//     console.log(query)
-//     if (query.length === 0) {
-//       searchResults.innerHTML = '';
-//       return;
-//     }
-
-
-
-
-
-//     query = encodeURIComponent(query)
-
-// handleSearch(query)
-
-
-//   });
   function displayResults(results) {
-    searchResults.innerHTML = '';
+    searchResultsRef.current.innerHTML = '';
 
     if (results.length === 0) {
-      searchResults.innerHTML = 'No results found';
+      searchResultsRef.current.innerHTML = 'No results found';
       return;
     }
 
     const ul = document.createElement('ul');
     results.forEach(product => {
       const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href=`/product/${product.slug.current}`
+      a.appendChild(li)
       li.textContent = product.name;
-      ul.appendChild(li);
+      li.dataset.slug=product.slug.current;
+      ul.appendChild(a);
     });
-
-    searchResults.appendChild(ul);
+console.log(searchResultsRef.current)
+    searchResultsRef.current.appendChild(ul);
   }
 
 
@@ -87,32 +101,44 @@ function Navbar() {
 
 
 
+  <div></div>
 
-return (
-  <div className="navbar-container">
-    <div className="logo">
-      <Link style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} href="https://wa.me/+21652344555"><p>UZL</p>
-        <AiOutlineWhatsApp />
-      </Link>
+  return (
+    <div className="navbar-container">
+      <div className="logo">
+        <Link style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} href="https://wa.me/+21652344555"><p>UZL</p>
+          <AiOutlineWhatsApp />
+        </Link>
 
-    </div>
-    <div>
-      <div style={{ display: "flex", position: 'relative', justifyContent: 'center', alignItems: 'center  ' }}>
-        <input type="text" id="searchInput" placeholder="Search products..." />
-        <AiOutlineSearch style={{ position: 'absolute', right: '0' }} />
       </div>
-      <div id="searchResults"></div>
-    </div>
-    <button className="cart-icon" onClick={() => setShowCart(true)}>
+      <div>
+        <div style={{ display: "flex", position: 'relative', justifyContent: 'center', alignItems: 'center  ' }}>
+          <input type="text" id="searchInput" ref={searchInputRef} placeholder="Search products..." onKeyDown={handleSearch}/>
+          <AiOutlineSearch style={{ position: 'absolute', right: '0' }} />
+        </div><div>
+        <div id="searchResults" ref={searchResultsRef}>
 
-      <AiOutlineShopping />
-      <span className="cart-item-qty">
-        {totalQuantities}
-      </span>
-    </button>
-    {showCart && <Cart />}
-  </div>
-)
+
+
+        </div>
+        {searchResults&&<AiOutlineClose onClick={()=>{
+          searchResultsRef.current.innerHTML="";
+
+setSearchResults(false)
+
+        }}/>}
+        </div>
+      </div>
+      <button className="cart-icon" onClick={() => setShowCart(true)}>
+
+        <AiOutlineShopping />
+        <span className="cart-item-qty">
+          {totalQuantities}
+        </span>
+      </button>
+      {showCart && <Cart />}
+    </div>
+  )
 }
 
 export default Navbar
