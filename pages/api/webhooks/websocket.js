7404a048bd79a122f830038fff3d7b09c804f255
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-20 17:28:50 +0100

    commit to main

--- pages/api/webhooks/websocket.js
+++ pages/api/webhooks/websocket.js
@@ -23,25 +23,6 @@ wss.on('connection', function connection(ws) {
 });
 
 // Handle incoming webhook events from Sanity.io
-server.post('/api/webhooks/websocket', (req, res) => {
-
-  // Process the webhook event from Sanity.io
-  // // Broadcast the event over WebSocket to connected clients
-  // console.log('Received webhook event from Sanity.io');
-
-  // // Handle the webhook payload and broadcast to WebSocket clients
-
-  // res.status(200).json({ message: 'Webhook received successfully' });
-
-
-  wss.clients.forEach((client) => {
-    if (client.readyState === 1) { // Corrected typo here
-      client.send('Dataset updated'); // You can send any information you want here
-    }
-  });
-
-});
-
 
 httpServer.listen(3000, () => {
   console.log(`Next.js server with WebSocket running on port 3000`);
