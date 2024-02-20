4f92dd3a1c5ef9298a368e228a899ad6b19e0551
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-20 20:59:42 +0100

    commit to main

--- pages/api/websocket.mjs
+++ pages/api/websocket.mjs
@@ -1,25 +1,25 @@
-// export default async (req, res) => {
-//   try {
-//     if (req.method !== "POST") {
-//       res.status(405).json({ error: "Method Not Allowed" });
-//       return;
-//     }
+export default async (req, res) => {
+  try {
+    if (req.method !== "POST") {
+      res.status(405).json({ error: "Method Not Allowed" });
+      return;
+    }
 
-//     // Process the webhook payload
-//     const payload = req.body; // Assuming the payload is in the request body
+    // Process the webhook payload
+    const payload = req.body; // Assuming the payload is in the request body
 
-//     // Do something with the payload
-//     console.log("Webhook received:", payload);
+    // Do something with the payload
+    console.log("Webhook received:", payload);
 
-//     // Return a response (optional)
-//     res.status(200).json({ message: "Webhook received successfully!" });
-//   } catch (error) {
-//     console.error("Webhook error:", error);
-//     res
-//       .status(500)
-//       .json({ error: "An error occurred while processing the webhook." });
-//   }
-// };
+    // Return a response (optional)
+    res.status(200).json({ message: "Webhook received successfully!" });
+  } catch (error) {
+    console.error("Webhook error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the webhook." });
+  }
+};
 
 
 
@@ -68,56 +68,56 @@
 
 
 
-import { EventEmitter } from 'events';
+// import { EventEmitter } from 'events';
 
-const webhookEmitter = new EventEmitter();
+// const webhookEmitter = new EventEmitter();
 
-export default async function handler(req, res) {
-  try {
-    if (req.method !== "POST") {
-      res.status(405).json({ error: "Method Not Allowed" });
-      return;
-    }
+// export default async function handler(req, res) {
+//   try {
+//     if (req.method !== "POST") {
+//       res.status(405).json({ error: "Method Not Allowed" });
+//       return;
+//     }
 
-    // Process the webhook payload
-    const payload = req.body; // Assuming the payload is in the request body
+//     // Process the webhook payload
+//     const payload = req.body; // Assuming the payload is in the request body
 
-    // Emit an SSE event with the payload data
-    webhookEmitter.emit('webhookReceived', payload);
+//     // Emit an SSE event with the payload data
+//     webhookEmitter.emit('webhookReceived', payload);
 
-    // Return a response (optional)
-    res.status(200).json({ message: "Webhook received successfully!" });
-  } catch (error) {
-    console.error("Webhook error:", error);
-    res.status(500).json({ error: "An error occurred while processing the webhook." });
-  }
-}
+//     // Return a response (optional)
+//     res.status(200).json({ message: "Webhook received successfully!" });
+//   } catch (error) {
+//     console.error("Webhook error:", error);
+//     res.status(500).json({ error: "An error occurred while processing the webhook." });
+//   }
+// }
 
-// Function to handle SSE connections
-export const streamEvents = (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
+// // Function to handle SSE connections
+// export const streamEvents = (req, res) => {
+//   res.setHeader('Content-Type', 'text/event-stream');
+//   res.setHeader('Cache-Control', 'no-cache');
+//   res.setHeader('Connection', 'keep-alive');
 
-  // Function to send SSE events
-  const sendEvent = (data) => {
-    res.write(`data: ${JSON.stringify(data)}\n\n`);
-  };
+//   // Function to send SSE events
+//   const sendEvent = (data) => {
+//     res.write(`data: ${JSON.stringify(data)}\n\n`);
+//   };
 
-  // Listen for webhook events
-  webhookEmitter.on('webhookReceived', (data) => {
-    sendEvent(data);
-  });
+//   // Listen for webhook events
+//   webhookEmitter.on('webhookReceived', (data) => {
+//     sendEvent(data);
+//   });
 
-  // Keep the connection alive
-  const intervalId = setInterval(() => {
-    res.write(': ping\n\n'); // Send a "ping" event every few seconds to keep the connection alive
-  }, 10000);
+//   // Keep the connection alive
+//   const intervalId = setInterval(() => {
+//     res.write(': ping\n\n'); // Send a "ping" event every few seconds to keep the connection alive
+//   }, 10000);
 
-  // Close the SSE connection when the client disconnects
-  req.on('close', () => {
-    clearInterval(intervalId); // Clear the interval
-    webhookEmitter.off('webhookReceived', sendEvent); // Remove the event listener
-    res.end(); // End the response
-  });
-};
+//   // Close the SSE connection when the client disconnects
+//   req.on('close', () => {
+//     clearInterval(intervalId); // Clear the interval
+//     webhookEmitter.off('webhookReceived', sendEvent); // Remove the event listener
+//     res.end(); // End the response
+//   });
+// };
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,59 +1,14 @@
-"use client"
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { client } from '../../lib/client'
 import Layout from '../../src/app/components/Layout'; // Import the Layout component
 import Product from '../../src/app/components/Product';
-import { useRouter } from 'next/router';
 
 const CategoryProducts = ({ categoryProducts }) => {
-
-    const [products, setProducts] = useState([]);
-    const [datasetUpdated, setDatasetUpdated] = useState(false);
-
-
-    // Inside your functional component
-    const router = useRouter();
-    const { slug } = router.query;
-    console.log(slug)
-
-    useEffect(() => {
-        const eventSource = new EventSource('/api/websocket');
-
-        eventSource.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            console.log(data)
-            if (data._type === slug) {
-                fetchProductsByCategory(slug);
-            }
-        };
-
-        eventSource.onerror = (error) => {
-            console.error('SSE error:', error);
-        };
-
-        return () => {
-            eventSource.close();
-        };
-    }, [slug]);
-
-    const fetchProductsByCategory = async (categorySlug) => {
-        try {
-            const productsQuery = `*[_type == "${categorySlug}"]`;
-            const products = await client.fetch(productsQuery);
-            setProducts(products);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        }
-    };
-
-
-
-
-    // console.log(categoryProducts)
+    console.log(categoryProducts)
     return (
         <Layout>
             <div className="products-container">
-                {products?.map((item) => (
+                {categoryProducts?.map((item) => (
                     <Product key={item._id} product={item} />
                 ))}
             </div>
@@ -73,10 +28,9 @@ const CategoryProducts = ({ categoryProducts }) => {
 
 
 export const getStaticProps = async ({ params: { slug } }) => {
-    // console.log(slug)
+    console.log(slug)
     const productsQuery = `*[_type =="${slug}"]`
     const categoryProducts = await client.fetch(productsQuery)
-
     return {
         props: { categoryProducts }
     }
