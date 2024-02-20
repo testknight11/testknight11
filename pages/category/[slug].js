a94b4942f00c38fcc8936d42ac6a1d60336c3822
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-20 17:08:42 +0100

    commit to main

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -18,7 +18,7 @@ const CategoryProducts = ({ categoryProducts }) => {
     useEffect(() => {
         setProducts(categoryProducts)
         // Establish WebSocket connection
-        const ws = new WebSocket(`wss://ecommerce-r126.vercel.app/api/webhooks/websocket`); // Update with your WebSocket server URL
+        const ws = new WebSocket(`wss://ecommerce-r126.vercel.app:3000/api/webhooks/websocket`); // Update with your WebSocket server URL
 
         // WebSocket event listeners
         ws.onopen = () => {
