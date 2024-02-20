099d485aa27a862499ecdc658bfde80c63b7682d
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-20 21:08:23 +0100

    commit to main

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,14 +1,59 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
 import { client } from '../../lib/client'
 import Layout from '../../src/app/components/Layout'; // Import the Layout component
 import Product from '../../src/app/components/Product';
+import { useRouter } from 'next/router';
 
 const CategoryProducts = ({ categoryProducts }) => {
-    console.log(categoryProducts)
+
+    const [products, setProducts] = useState([]);
+    const [datasetUpdated, setDatasetUpdated] = useState(false);
+
+
+    // Inside your functional component
+    const router = useRouter();
+    const { slug } = router.query;
+    console.log(slug)
+
+    useEffect(() => {
+        const eventSource = new EventSource('/api/websocket');
+
+        eventSource.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log(data)
+            if (data._type === slug) {
+                fetchProductsByCategory(slug);
+            }
+        };
+
+        eventSource.onerror = (error) => {
+            console.error('SSE error:', error);
+        };
+
+        return () => {
+            eventSource.close();
+        };
+    }, [slug]);
+
+    const fetchProductsByCategory = async (categorySlug) => {
+        try {
+            const productsQuery = `*[_type == "${categorySlug}"]`;
+            const products = await client.fetch(productsQuery);
+            setProducts(products);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+        }
+    };
+
+
+
+
+    // console.log(categoryProducts)
     return (
         <Layout>
             <div className="products-container">
-                {categoryProducts?.map((item) => (
+                {products?.map((item) => (
                     <Product key={item._id} product={item} />
                 ))}
             </div>
@@ -28,9 +73,10 @@ const CategoryProducts = ({ categoryProducts }) => {
 
 
 export const getStaticProps = async ({ params: { slug } }) => {
-    console.log(slug)
+    // console.log(slug)
     const productsQuery = `*[_type =="${slug}"]`
     const categoryProducts = await client.fetch(productsQuery)
+
     return {
         props: { categoryProducts }
     }
