36b07b7ed9053bba73bb721474aadc5fed599cff
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-20 17:04:17 +0100

    commit to main

--- pages/api/webhooks/websocket.js
+++ pages/api/webhooks/websocket.js
@@ -11,7 +11,7 @@ const handle = app.getRequestHandler();
 
 const server = express();
 const httpServer = https.createServer(server); // Create HTTP server using Express app
-const wss = new WebSocketServer.Server({ server: httpServer }); 
+const wss = new WebSocketServer.Server({ server: httpServer });
 
 // Handle WebSocket connections
 wss.on('connection', function connection(ws) {
@@ -25,23 +25,23 @@ wss.on('connection', function connection(ws) {
 // Handle incoming webhook events from Sanity.io
 server.post('/api/webhooks/websocket', (req, res) => {
 
-    // Process the webhook event from Sanity.io
-    // // Broadcast the event over WebSocket to connected clients
-    // console.log('Received webhook event from Sanity.io');
-    
-    // // Handle the webhook payload and broadcast to WebSocket clients
+  // Process the webhook event from Sanity.io
+  // // Broadcast the event over WebSocket to connected clients
+  // console.log('Received webhook event from Sanity.io');
 
-    // res.status(200).json({ message: 'Webhook received successfully' });
+  // // Handle the webhook payload and broadcast to WebSocket clients
+
+  // res.status(200).json({ message: 'Webhook received successfully' });
 
 
-      wss.clients.forEach((client) => {
+  wss.clients.forEach((client) => {
     if (client.readyState === 1) { // Corrected typo here
       client.send('Dataset updated'); // You can send any information you want here
     }
   });
 
 });
-P
+
 
 httpServer.listen(443, () => {
   console.log(`Next.js server with WebSocket running on port ${PORT}`);
