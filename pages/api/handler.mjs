e6e50f200c2edb0952d03b14059a7012a200f2fe
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-21 18:22:23 +0100

    commit to main

--- pages/api/handler.mjs
+++ pages/api/handler.mjs
@@ -1,21 +1,22 @@
-import { EventEmitter } from 'events';
+import EventEmitter from 'events';
 
 export const webhookEmitter = new EventEmitter();
 
 
 export default function handler(req, res) {
   try {
+    if (req.method === "POST") {
+      // Process the webhook payload
+      const payload = req.body; // Assuming the payload is in the request body
 
-    console.log(req)
-    // Process the webhook payload
-    const payload = req.body; // Assuming the payload is in the request body
-
-    // Emit an SSE event with the payload data
-    webhookEmitter.emit('webhookReceived', payload)
-    // Return a success response
-    res.status(200).json({ message: 'Webhook received successfully!' });
-  }
-  catch (error) {
+      // Emit an SSE event with the payload data
+      webhookEmitter.emit('webhookReceived',payload)
+      // Return a success response
+      res.status(200).json({ message: 'Webhook received successfully!' });
+    } else {
+      res.status(405).json({ error: "Method Not Allowed" });
+    }
+  } catch (error) {
     console.error('Webhook error:', error);
     res.status(500).json({ error: 'An error occurred while processing the webhook.' });
   }
