f0fb601e1dd8a2380378034013ae4a92848707ac
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-21 02:12:54 +0100

    commit to main

--- pages/api/sseHandler.js
+++ pages/api/sseHandler.js
@@ -4,33 +4,35 @@ import { webhookEmitter } from './handler.js';
 
 export default function sseHandler(req, res) {
   console.log(webhookEmitter)
-  if (req.headers.accept && req.headers.accept.includes('text/event-stream')) {
-    // Set SSE headers
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
+  if (webhookEmitter.listenerCount('webhookReceived') > 0 && webhookEmitter.listenerCount('webhookReceived') > 0) {
+    if (req.headers.accept && req.headers.accept.includes('text/event-stream')) {
+      // Set SSE headers
+      res.setHeader('Content-Type', 'text/event-stream');
+      res.setHeader('Cache-Control', 'no-cache');
+      res.setHeader('Connection', 'keep-alive');
 
-    // Keep the connection alive
-    const intervalId = setInterval(() => {
-      res.write(': ping\n\n'); // Send a "ping" event every few seconds to keep the connection alive
-    }, 10000);
+      // Keep the connection alive
+      const intervalId = setInterval(() => {
+        res.write(': ping\n\n'); // Send a "ping" event every few seconds to keep the connection alive
+      }, 10000);
 
-    const sendEvent = (data) => {
-      res.write(`data: ${JSON.stringify(data)}\n\n`);
-    };
+      const sendEvent = (data) => {
+        res.write(`data: ${JSON.stringify(data)}\n\n`);
+      };
 
-    // Listen for webhook events
-    webhookEmitter.on('webhookReceived', (data) => {
-      sendEvent(data);
-    });
+      // Listen for webhook events
+      webhookEmitter.on('webhookReceived', (data) => {
+        sendEvent(data);
+      });
 
-    req.socket.on('close', () => {
-      clearInterval(intervalId);
-      webhookEmitter.off('webhookReceived', sendEvent);
-      res.end();
-    });
-  } else {
-    // Handle requests that don't accept SSE
-    res.status(400).end();
+      req.socket.on('close', () => {
+        clearInterval(intervalId);
+        webhookEmitter.off('webhookReceived', sendEvent);
+        res.end();
+      });
+    } else {
+      // Handle requests that don't accept SSE
+      res.status(400).end();
+    }
   }
 }
