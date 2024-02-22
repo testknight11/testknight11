c102959e021250dc48415144c2ad93abe0132035
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-22 12:00:58 +0100

    commit to main

--- pages/api/handler.js
+++ pages/api/handler.js
@@ -6,14 +6,14 @@ import { EventEmitter } from '@foxify/events';
 //   // Process the payload data here
 // });
 
-export let webhookEmitter;
+
 
 // Example: Listening for an event
 
 
 export default async function handler(req, res) {
   try {
-    webhookEmitter = new EventEmitter();
+    const webhookEmitter = new EventEmitter();
     webhookEmitter.on('webhookReceived', (payload) => {
       console.log('Received webhook data:', payload);
       // Process the payload data here
