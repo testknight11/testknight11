8d8eac76fe1b7c984f0339beb6166492dd87a3b6
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-22 23:27:03 +0100

    commit to main

--- pages/api/handler.js
+++ pages/api/handler.js
@@ -64,7 +64,7 @@ export default async function handler(req, res) {
       console.log('test1')
 
       // Return a success response
-    res.status(200).json({ message: 'Webhook received test successfully!' });
+    // res.status(200).json({ message: 'Webhook received test successfully!' });
 
     }
 
