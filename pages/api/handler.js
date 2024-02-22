2bd514b3c49ecd3189733c8dd0b7f08565d65cf5
moorsknight11 <hajjejhazem063@gmail.com>
2024-02-22 17:48:00 +0100

    commit to main

--- pages/api/handler.js
+++ pages/api/handler.js
@@ -85,7 +85,7 @@ export default async function handler(req, res) {
         console.log('tet3')
 
 
-
+        await res.status(200).json('it happened');
       } else {
         // Handle requests that don't accept SSE
         console.log('tesssssssssssssssssssssst 400')
