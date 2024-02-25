0d4bc58bef0f5b374317964ce8d27f3b0d4cc2a3
testknight11 <altinsoylar11@gmail.com>
2024-02-25 22:11:32 +0100

    first commit

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -40,15 +40,15 @@ const ProductDetails = ({ product, products }) => {
     const router = useRouter();
     const { slug } = router.query;
     const { image, name, details, price, prices, _type, colors, _id } = savedProduct ? product : "";
-    // useEffect(() => {
+    useEffect(() => {
 
-    //     setTestImage([...image])
-    //     setTestName(name)
-    //     setTestDetails(details)
+        setTestImage([...image])
+        setTestName(name)
+        setTestDetails(details)
 
 
 
-    // }, [image, name, details])
+    }, [image, name, details])
     useEffect(() => {
         console.log(product)
         setSavedProduct(product)
